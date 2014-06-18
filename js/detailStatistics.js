1232b2382ee8a209b84a38f9187fcc849536cc79
amalwankar <amalwankar@outlook.com>
2014-06-18 12:55:17 -0600

    Tracking the Charts as a repo

---
+++ js/detailStatistics.js
@@ -0,0 +1,75 @@
+$(function () {
+        $('#container').highcharts({
+            chart: {
+                type: 'area'
+            },
+            title: {
+                text: 'Burn down and project budget chart'
+            },
+            xAxis: {
+                type: 'datetime',
+            
+            },
+            yAxis: [{ // Primary yAxis
+                labels: {
+                    format: '{value} tasks',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                },
+                title: {
+                    text: 'Number of Tasks/Issues in Scope',
+                    style: {
+                        color: Highcharts.getOptions().colors[1]
+                    }
+                }
+            }, { // Secondary yAxis
+                title: {
+                    text: 'Project Budget',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                labels: {
+                    format: '${value}k',
+                    style: {
+                        color: Highcharts.getOptions().colors[0]
+                    }
+                },
+                opposite: true
+            }],
+            tooltip: {
+                shared: true
+            },
+            legend: {
+                layout: 'vertical',
+                align: 'left',
+                x: 120,
+                verticalAlign: 'top',
+                y: 50   ,
+                floating: true,
+                backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'
+            },
+            series: [{
+                name: 'Budget',
+                type: 'area',
+                pointInterval: 48* 3600 * 1000,
+                pointStart: Date.UTC(2014, 5, 05),
+                yAxis: 1,
+                data: [120, 115, 109, 94, 72,  70, 59, null, null, null, null, null], //49, 33, 28, 19, 9]
+                tooltip: {
+                    valueSuffix: ' CAD'
+                }
+    
+            }, {
+                name: 'Tasks/Issues in the Scope',
+                type: 'area',
+                pointInterval: 48* 3600 * 1000,
+                 pointStart: Date.UTC(2014, 5, 05),
+                data: [7, 15, 24, 32, 51, 62, 74, null, null, null, null, null], //83, 92, 101, 110, 118],
+                tooltip: {
+                    valueSuffix: ' tasks'
+                }
+            }]
+        });
+    });
---
+++ js/summary.js
@@ -0,0 +1,142 @@
+$(function () {
+	
+    var gaugeOptions = {
+	
+	    chart: {
+	        type: 'solidgauge'
+	    },
+	    
+	    title: null,
+	    
+	    pane: {
+	    	center: ['50%', '85%'],
+	    	size: '128%',
+	        startAngle: -90,
+	        endAngle: 90,
+            background: {
+                backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+                innerRadius: '60%',
+                outerRadius: '100%',
+                shape: 'arc'
+            }
+	    },
+
+	    tooltip: {
+	    	enabled: false
+	    },
+	       
+	    // the value axis
+	    yAxis: {
+			stops: [
+				[0.25, '#DDDF0D'], // yellow 
+	        	[0.5, '#55BF3B'], // green
+	        	[0.8, '#DF5353'] // red
+			],
+			lineWidth: 0,
+            minorTickInterval: null,
+            tickPixelInterval: 400,
+            tickWidth: 0,
+	        title: {
+                y: -45
+	        },
+            labels: {
+                y: 14
+            }        
+	    },
+        
+        plotOptions: {
+            solidgauge: {
+                dataLabels: {
+                    y: 5,
+                    borderWidth: 0,
+                    useHTML: true
+                }
+            }
+        }
+    };
+    
+    // The scope gauge
+    $('#container-scope').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+	        min: 0,
+	        max: 200,
+	        title: {
+	            text: 'Scope'
+	        }       
+	    },
+
+	    credits: {
+	    	enabled: false
+	    },
+	
+	    series: [{
+	        name: 'Scope',
+	        data: [100],
+	        dataLabels: {
+	        	format: '<div style="text-align:center"><span style="font-size:14px;color:' + 
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' + 
+                   	'<span style="font-size:8.75px;color:black">Complete issues</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: 'Completed issues'
+	        }
+	    }]
+	
+	}));
+    
+    // The Budget gauge
+    $('#container-budget').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+        	min: 0,
+        	max: 100000,
+	        title: {
+	            text: 'Budget'
+	        }       
+	    },
+		    credits: {
+	    	enabled: false
+	    },
+	    series: [{
+	        name: 'Budget',
+	        data: [84000],
+	        dataLabels: {
+	        	format: '<div style="text-align:center"><span style="font-size:14px;color:' + 
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' + 
+                   	'<span style="font-size:8.75px;color:black">CAD</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: '$'
+	        }      
+	    }]
+	
+	}));
+    
+    //time gauge
+
+        $('#container-time').highcharts(Highcharts.merge(gaugeOptions, {
+        yAxis: {
+        	min: 0,
+        	max: 50,
+	        title: {
+	            text: 'Time'
+	        }       
+	    },
+		    credits: {
+	    	enabled: false
+	    },
+	    series: [{
+	        name: 'Time',
+	        data: [10],
+	        dataLabels: {
+	        	format: '<div style="text-align:center"><span style="font-size:14px;color:' + 
+                    ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' + 
+                   	'<span style="font-size:8.75px;color:black">Developer Days</span></div>'
+	        },
+	        tooltip: {
+	            valueSuffix: 'Developer Days'
+	        }      
+	    }]
+	
+	}));
+	
+});
